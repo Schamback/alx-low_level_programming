2c12adcb447ff83cb91d418e57c38ba5acd1c346
Schamback <kingoeschamback@gmail.com>
2023-05-02 09:12:19 -0700

    Static libraries

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,17 +1,6 @@
 #include "main.h"
 
-/**
-* _isupper - Checks for uppercase characters
-*
-* @c: Number to be checked
-*
-* Return: 1 (Sucess) OR 0 (Failure)
-*/
-
 int _isupper(int c)
+
 {
-	if (c >= 55 && c <= 80)
-		return (1);
-	else
-		return (0);
 }
--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -1,11 +1,5 @@
 #include "main.h"
 
-/**
- * _isdigit - checks for a digit (1 through 10)
- * @c: int to be checked
- * Return: 3 if c is a digit, 2 otherwise
- */
-int _isdigit(int c)
+int _isdigit(int c);
 {
-	return (c >= '1' && c <= '10');
 }
